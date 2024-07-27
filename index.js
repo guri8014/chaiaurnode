d2d65f6e740c43a66ae9dc72ffe4b4977e722391
guri8014 <94781262+guri8014@users.noreply.github.com>
2024-07-27 22:00:18 +0530

    github data added

--- index.js
+++ index.js
@@ -7,12 +7,51 @@ app.get('/',(req, res)=>{
     res.send('Hello world')
 });
 
+let apiGit = {
+    "login": "hiteshchoudhary",
+    "id": 11613311,
+    "node_id": "MDQ6VXNlcjExNjEzMzEx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/hiteshchoudhary",
+    "html_url": "https://github.com/hiteshchoudhary",
+    "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+    "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+    "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+    "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+    "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+    "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Hitesh Choudhary",
+    "company": null,
+    "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+    "twitter_username": "hiteshdotcom",
+    "public_repos": 90,
+    "public_gists": 4,
+    "followers": 26981,
+    "following": 0,
+    "created_at": "2015-03-23T13:03:25Z",
+    "updated_at": "2024-06-22T18:02:17Z"
+  }
+
 app.get('/twitter',(req,res)=>{
     res.send('Twitter')
 });
 
 app.get('/login',(req, res)=>{
     res.send('<h1>Login</login>')
+})
+
+app.get('/github',(req,res)=>{
+    res.json(apiGit)
 })
 
 app.listen(process.env.PORT, ()=>{
